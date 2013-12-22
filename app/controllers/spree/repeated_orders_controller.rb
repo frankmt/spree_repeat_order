706164fe1ab0c2ea9988e1540e8a9e05dcb09ef6
Francisco Trindade <frank.trindade@gmail.com>
2013-12-23 00:01:55 +1100

    mocked test for controller

--- app/controllers/spree/repeated_orders_controller.rb
+++ app/controllers/spree/repeated_orders_controller.rb
@@ -2,6 +2,17 @@ module Spree
   class RepeatedOrdersController < Spree::StoreController
 
     def create
+      duplicated_order = Spree::Order.find(params[:id])
+      new_order = current_order(true)
+
+      new_line_items = []
+      duplicated_order.line_items.each do |line_item|
+        new_line_items << line_item.dup
+      end
+
+      new_order.line_items = new_line_items
+      new_order.save
+
       render json: {}, status: 201
     end
 
--- spec/controllers/repeated_orders_controller_spec.rb
+++ spec/controllers/repeated_orders_controller_spec.rb
@@ -4,10 +4,31 @@ describe Spree::RepeatedOrdersController do
 
   describe "create" do
 
-    it 'should return 201' do
-      spree_post :create
+    let(:line_item_1){ FactoryGirl.build(:line_item) }
+    let(:line_item_2){ FactoryGirl.build(:line_item) }
+    let(:past_order){ FactoryGirl.build(:order, line_items: [line_item_1, line_item_2]) }
+
+    let(:new_order){ FactoryGirl.build(:order) }
+    let(:line_item_clone_1){ FactoryGirl.build(:line_item) }
+    let(:line_item_clone_2){ FactoryGirl.build(:line_item) }
+
+    before :each do
+      Spree::Order.stub(:find).and_return(past_order)
+    end
+
+    it 'should create new order with same line items' do
+      controller.should_receive(:current_order).and_return(new_order)
+      line_item_1.should_receive(:dup).and_return(line_item_clone_1)
+      line_item_2.should_receive(:dup).and_return(line_item_clone_2)
+
+      new_order.should_receive(:line_items=).with([line_item_clone_1, line_item_clone_2])
+      new_order.should_receive(:save).and_return(true)
+
+      spree_post :create, id: "ABC1"
       response.status.should == 201
     end
+
+
 
   end
 
