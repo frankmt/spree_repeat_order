09c5c3f6b2e729cd58184a0422e3594ce4ce0dcf
Steph Hof <steph.hof@gmail.com>
2017-08-11 11:54:58 +1000

    Fix up repeat order button deface for spree admin screens

--- app/overrides/repeat_order_button.rb
+++ app/overrides/repeat_order_button.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/admin/shared/_content_header",
                      :name => "repeat_order_button",
-                     :insert_top => "[data-hook='toolbar']>ul",
+                     :insert_top => "[data-hook='toolbar']",
                      :partial => "spree/admin/orders/repeat_button",
                      :disabled => false)
